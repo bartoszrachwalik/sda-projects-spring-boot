8a4ecd6c5d50f28f8cf9e6857bd1b452f4789528
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-20 20:43:22 +0100

    adds SecurityConfig

--- src/main/java/com/github/springboot/Application.java
+++ src/main/java/com/github/springboot/Application.java
@@ -1,9 +1,12 @@
 package com.github.springboot;
 
+import com.github.springboot.security.application.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SecurityConfig.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
